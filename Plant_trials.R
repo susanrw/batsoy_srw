d9f4dd35dfce38fe4883c1564056b8c7b8c6eb42
ldmaynard <ladamay01@gmail.com>
2021-08-19 18:15:24 -0400

    new data and data cleaning/wrangling

--- Plant_trials.R
+++ Plant_trials.R
@@ -1,10 +1,142 @@
+w1 <- read.csv(file="id_w1.csv",head=TRUE)
+w2 <- read.csv(file="id_w2.csv",head=TRUE)
+w3 <- read.csv(file="id_w3.csv",head=TRUE)
+w4 <- read.csv(file="id_w4.csv",head=TRUE)
+e1 <- read.csv(file="id_e1.csv",head=TRUE)
+e2 <- read.csv(file="id_e2.csv",head=TRUE)
+e3 <- read.csv(file="id_e3.csv",head=TRUE)
+e4 <- read.csv(file="id_e4.csv",head=TRUE)
+
+#combine aggregated herbivory and chemistry datasets
+all.dat.w <- rbind(w1, w2, w3, w4)
+all.dat.e <- rbind(e1, e3, e3, e4)
+
+library(dplyr)
+
+all.bat.w<-select(all.dat.w, DATE, TIME, HOUR, DATE.12, TIME.12, HOUR.12, AUTO.ID., FILES)
+all.bat.e<-select(all.dat.e, DATE, TIME, HOUR, DATE.12, TIME.12, HOUR.12, AUTO.ID., FILES)
+
+{	all.bat.w$AUTO.ID.<-as.factor(all.bat.w$AUTO.ID.)
+	all.bat.w$DATE.12<-as.Date(all.bat.w$DATE.12,"%m/%d/%y")
+	all.bat.w$HOUR<-as.numeric(all.bat.w$HOUR)
+	all.bat.w$DATE<-as.Date(all.bat.w$DATE,"%m/%d/%y")
+	all.bat.w$HOUR.12<-as.numeric(all.bat.w$HOUR.12)
+}
+
+{	all.bat.e$AUTO.ID.<-as.factor(all.bat.e$AUTO.ID.)
+	all.bat.e$DATE.12<-as.Date(all.bat.e$DATE.12,"%m/%d/%y")
+	all.bat.e$HOUR<-as.numeric(all.bat.e$HOUR)
+	all.bat.e$DATE<-as.Date(all.bat.e$DATE,"%m/%d/%y")
+	all.bat.e$HOUR.12<-as.numeric(all.bat.e$HOUR.12)
+}
+
+#EASTSIDE
+all.bat.e$treatment <- NA
+for(i in 1:length(all.bat.e$DATE.12)){
+	if(all.bat.e$DATE.12[i]=="2021-07-17"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-18"){all.bat.e$treatment[i]="D"}
+	if(all.bat.e$DATE.12[i]=="2021-07-19"){all.bat.e$treatment[i]="D"}
+	if(all.bat.e$DATE.12[i]=="2021-07-20"){all.bat.e$treatment[i]="U"}
+	if(all.bat.e$DATE.12[i]=="2021-07-21"){all.bat.e$treatment[i]="U"}
+	if(all.bat.e$DATE.12[i]=="2021-07-22"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-23"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-24"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-25"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-26"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-27"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-28"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-29"){all.bat.e$treatment[i]="R"}
+	if(all.bat.e$DATE.12[i]=="2021-07-30"){all.bat.e$treatment[i]="U"}
+	if(all.bat.e$DATE.12[i]=="2021-07-31"){all.bat.e$treatment[i]="U"}
+	if(all.bat.e$DATE.12[i]=="2021-08-01"){all.bat.e$treatment[i]="D"}
+	if(all.bat.e$DATE.12[i]=="2021-08-02"){all.bat.e$treatment[i]="D"}
+	if(all.bat.e$DATE.12[i]=="2021-08-03"){all.bat.e$treatment[i]="D"}
+	if(all.bat.e$DATE.12[i]=="2021-08-04"){all.bat.e$treatment[i]="D"}
+	
+}
+
+all.bat.e$trial <- NA
+for(i in 1:length(all.bat.e$DATE.12)){
+	if(all.bat.e$DATE.12[i]=="2021-07-17"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-18"){all.bat.e$trial[i]="1"}
+	if(all.bat.e$DATE.12[i]=="2021-07-19"){all.bat.e$trial[i]="1"}
+	if(all.bat.e$DATE.12[i]=="2021-07-20"){all.bat.e$trial[i]="2"}
+	if(all.bat.e$DATE.12[i]=="2021-07-21"){all.bat.e$trial[i]="2"}
+	if(all.bat.e$DATE.12[i]=="2021-07-22"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-23"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-24"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-25"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-26"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-27"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-28"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-29"){all.bat.e$trial[i]="N/A"}
+	if(all.bat.e$DATE.12[i]=="2021-07-30"){all.bat.e$trial[i]="3"}
+	if(all.bat.e$DATE.12[i]=="2021-07-31"){all.bat.e$trial[i]="3"}
+	if(all.bat.e$DATE.12[i]=="2021-08-01"){all.bat.e$trial[i]="4"}
+	if(all.bat.e$DATE.12[i]=="2021-08-02"){all.bat.e$trial[i]="4"}
+	if(all.bat.e$DATE.12[i]=="2021-08-03"){all.bat.e$trial[i]="5"}
+	if(all.bat.e$DATE.12[i]=="2021-08-04"){all.bat.e$trial[i]="5"}
+	
+}
+
+#WESTSIDE
+all.bat.w$treatment <- NA
+for(i in 1:length(all.bat.w$DATE.12)){
+	if(all.bat.w$DATE.12[i]=="2021-07-17"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-18"){all.bat.w$treatment[i]="U"}
+	if(all.bat.w$DATE.12[i]=="2021-07-19"){all.bat.w$treatment[i]="U"}
+	if(all.bat.w$DATE.12[i]=="2021-07-20"){all.bat.w$treatment[i]="D"}
+	if(all.bat.w$DATE.12[i]=="2021-07-21"){all.bat.w$treatment[i]="D"}
+	if(all.bat.w$DATE.12[i]=="2021-07-22"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-23"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-24"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-25"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-26"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-27"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-28"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-29"){all.bat.w$treatment[i]="R"}
+	if(all.bat.w$DATE.12[i]=="2021-07-30"){all.bat.w$treatment[i]="D"}
+	if(all.bat.w$DATE.12[i]=="2021-07-31"){all.bat.w$treatment[i]="D"}
+	if(all.bat.w$DATE.12[i]=="2021-08-01"){all.bat.w$treatment[i]="U"}
+	if(all.bat.w$DATE.12[i]=="2021-08-02"){all.bat.w$treatment[i]="U"}
+	if(all.bat.w$DATE.12[i]=="2021-08-03"){all.bat.w$treatment[i]="U"}
+	if(all.bat.w$DATE.12[i]=="2021-08-04"){all.bat.w$treatment[i]="U"}
+	
+}
+
+all.bat.w$trial <- NA
+for(i in 1:length(all.bat.w$DATE.12)){
+	if(all.bat.w$DATE.12[i]=="2021-07-17"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-18"){all.bat.w$trial[i]="1"}
+	if(all.bat.w$DATE.12[i]=="2021-07-19"){all.bat.w$trial[i]="1"}
+	if(all.bat.w$DATE.12[i]=="2021-07-20"){all.bat.w$trial[i]="2"}
+	if(all.bat.w$DATE.12[i]=="2021-07-21"){all.bat.w$trial[i]="2"}
+	if(all.bat.w$DATE.12[i]=="2021-07-22"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-23"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-24"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-25"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-26"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-27"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-28"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-29"){all.bat.w$trial[i]="N/A"}
+	if(all.bat.w$DATE.12[i]=="2021-07-30"){all.bat.w$trial[i]="3"}
+	if(all.bat.w$DATE.12[i]=="2021-07-31"){all.bat.w$trial[i]="3"}
+	if(all.bat.w$DATE.12[i]=="2021-08-01"){all.bat.w$trial[i]="4"}
+	if(all.bat.w$DATE.12[i]=="2021-08-02"){all.bat.w$trial[i]="4"}
+	if(all.bat.w$DATE.12[i]=="2021-08-03"){all.bat.w$trial[i]="5"}
+	if(all.bat.w$DATE.12[i]=="2021-08-04"){all.bat.w$trial[i]="5"}
+	
+}
+
+all.bat <- rbind(all.bat.e, all.bat.w)
+
 bat1 <- read.csv(file="Trial 1.csv",head=TRUE)
 
 library(lme4)
 
 bat1<-bat1[1:899,]
 
-bat1 <- bat1[order(bat1$Treatment),]
+bat1 <- bat1[order(bat1$trial),]
 
 {
 bat1$AUTO.ID.<-as.factor(bat1$AUTO.ID.)
