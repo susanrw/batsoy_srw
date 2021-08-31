f03d0759e1b655d2547631dbaeb470f87681f1ef
ldmaynard <ladamay01@gmail.com>
2021-08-31 15:35:20 -0400

    reading in data and creating some plots

--- beanDIP.R
+++ beanDIP.R
@@ -20,3 +20,166 @@ bean %>%
 		 color="Site")+
 	theme_classic()
 #	
+
+#read in all data
+{ wy1 <- read.csv(file="id_Wye1.csv",head=TRUE)
+	wy2 <- read.csv(file="id_Wye2.csv",head=TRUE)
+	wy3 <- read.csv(file="id_Wye3.csv",head=TRUE)
+	wy4 <- read.csv(file="id_Wye4.csv",head=TRUE)
+	cv1 <- read.csv(file="id_Cv1.csv",head=TRUE)
+	cv2 <- read.csv(file="id_Cv2.csv",head=TRUE)
+	cv3 <- read.csv(file="id_Cv3.csv",head=TRUE)
+	cv4 <- read.csv(file="id_Cv4.csv",head=TRUE) }
+
+#Wye data
+wye.bean <- rbind(wy1, wy2, wy3, wy4)
+#Cville data
+cv.bean <- rbind(cv1, cv2, cv3, cv4)
+
+#creating site columns
+wye.bean$site <- NA
+for(i in 1:length(wye.bean$FILES)){
+	if(wye.bean$FILES[i]==1){wye.bean$site[i]="Wye"}
+}
+
+cv.bean$site <- NA
+for(i in 1:length(cv.bean$FILES)){
+	if(cv.bean$FILES[i]==1){cv.bean$site[i]="Clarksville"}
+}
+
+#removing noise files
+cv.bean<-cv.bean[cv.bean$AUTO.ID. != "Noise", ] 
+wye.bean<-wye.bean[wye.bean$AUTO.ID. != "Noise", ] 
+
+#selecting columns (need dplyr loaded)
+cv.bean<-select(cv.bean, DATE, TIME, HOUR, DATE.12, TIME.12, HOUR.12, AUTO.ID., FILES, site)
+wye.bean<-select(wye.bean, DATE, TIME, HOUR, DATE.12, TIME.12, HOUR.12, AUTO.ID., FILES, site)
+
+#formatting Cville
+{	cv.bean$AUTO.ID.<-as.factor(cv.bean$AUTO.ID.)
+	cv.bean$DATE.12<-as.Date(cv.bean$DATE.12,"%Y-%m-%d")
+	cv.bean$HOUR<-as.numeric(cv.bean$HOUR)
+	cv.bean$DATE<-as.Date(cv.bean$DATE,"%Y-%m-%d")
+	cv.bean$HOUR.12<-as.numeric(cv.bean$HOUR.12)
+}
+
+#formatting Wye
+{	wye.bean$AUTO.ID.<-as.factor(wye.bean$AUTO.ID.)
+	wye.bean$DATE.12<-as.Date(wye.bean$DATE.12,"%Y-%m-%d")
+	wye.bean$HOUR<-as.numeric(wye.bean$HOUR)
+	wye.bean$DATE<-as.Date(wye.bean$DATE,"%Y-%m-%d")
+	wye.bean$HOUR.12<-as.numeric(wye.bean$HOUR.12)
+}
+
+#aggregating data
+wye.bean.agg<-aggregate(FILES ~ DATE.12 + site + AUTO.ID., data=wye.bean, FUN=sum)
+cv.bean.agg<-aggregate(FILES ~ DATE.12 + site + AUTO.ID. , data=cv.bean, FUN=sum)
+
+#Wye plot
+plot.w1<-wye.bean.agg %>%
+	ggplot(aes(x=DATE.12, 
+			   y=FILES,
+			   color=AUTO.ID.))+
+	geom_point()+
+	geom_smooth()+
+	labs(x="Date",
+		 y="Rlative activity (no. nightly detections)",
+		 color="Site")+
+	theme_classic()
+plot.w1
+plot.w1+facet_wrap(~AUTO.ID.)
+
+#Cville plot
+plot.c1<-cv.bean.agg %>%
+	ggplot(aes(x=DATE.12, 
+			   y=FILES,
+			   color=AUTO.ID.))+
+	geom_point()+
+	geom_smooth()+
+	labs(x="Date",
+		 y="Rlative activity (no. nightly detections)",
+		 color="Site")+
+	theme_classic()
+plot.c1
+plot.c1+facet_wrap(~AUTO.ID.)
+
+#aggregating data by night/hour
+wye.bean.agg.h<-aggregate(FILES ~ DATE.12 + site + AUTO.ID. + HOUR.12, data=wye.bean, FUN=sum)
+cv.bean.agg.h<-aggregate(FILES ~ DATE.12 + site + AUTO.ID. + HOUR.12, data=cv.bean, FUN=sum)
+
+#Wye plot-night
+plot.w2<-wye.bean.agg.h %>%
+	ggplot(aes(x=HOUR.12,y=FILES))+
+	geom_point(aes(color=DATE.12))+
+	theme_classic()
+plot.w2
+plot.w2+geom_smooth(aes(group=DATE.12))
+plot.w2+geom_smooth()
+plot.w2+geom_smooth()+
+	facet_wrap(~AUTO.ID.)
+
+#Cville plot-night
+plot.c2<-cv.bean.agg.h %>%
+	ggplot(aes(x=HOUR.12,y=FILES))+
+	geom_point(aes(color=DATE.12))+
+	theme_classic()
+plot.c2
+plot.c2+geom_smooth(aes(group=DATE.12))
+plot.c2+geom_smooth()
+plot.c2+geom_smooth()+
+	facet_wrap(~AUTO.ID.)
+
+#combining into one dataset
+all.bean <- rbind(wye.bean, cv.bean)
+
+
+#formatting
+{	all.bean$AUTO.ID.<-as.factor(all.bean$AUTO.ID.)
+	all.bean$DATE.12<-as.Date(all.bean$DATE.12,"%Y-%m-%d")
+	all.bean$HOUR<-as.numeric(all.bean$HOUR)
+	all.bean$DATE<-as.Date(all.bean$DATE,"%Y-%m-%d")
+	all.bean$HOUR.12<-as.numeric(all.bean$HOUR.12)
+}
+
+all.bean.agg<-aggregate(FILES ~ DATE.12 + site + AUTO.ID., data=all.bean, FUN=sum)
+
+plot.all1<-all.bean.agg %>%
+	ggplot(aes(x=DATE.12, 
+			   y=FILES,
+			   color=site))+
+	geom_point()+
+	geom_smooth()+
+	labs(x="Date",
+		 y="Rlative activity (no. nightly detections)",
+		 color="Site")+
+	theme_classic()
+plot.all1
+plot.all1+facet_wrap(~AUTO.ID.)
+plot.all1+facet_wrap(~site)
+
+all.bean.agg.h<-aggregate(FILES ~ DATE.12 + site + AUTO.ID. + HOUR.12, data=all.bean, FUN=sum)
+
+#All plot-night
+plot.all2<-all.bean.agg.h %>%
+	ggplot(aes(x=HOUR.12,y=FILES))+
+	geom_point(aes(color=DATE.12))+
+	theme_classic()
+plot.all2
+plot.all2+geom_smooth(aes(group=site))
+plot.all2+geom_smooth(aes(group=site))+
+	facet_wrap(~AUTO.ID.)
+plot.all2+geom_smooth()+
+	facet_wrap(~site)
+
+#aggregating without species
+all.bean.agg.h2<-aggregate(FILES ~ DATE.12 + site + HOUR.12, data=all.bean, FUN=sum)
+
+#All plot-night
+plot.all3<-all.bean.agg.h2 %>%
+	ggplot(aes(x=HOUR.12,y=FILES))+
+	geom_point(aes(color=DATE.12))+
+	theme_classic()
+plot.all3
+plot.all3+geom_smooth(aes(group=site))
+plot.all3+geom_smooth()+
+	facet_wrap(~site)
